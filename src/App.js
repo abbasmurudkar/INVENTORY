de71be6601f54809635ed994317f2e71d42fcc15
abbasmurudkar <abbasmurudkar18@icloud.com>
2021-12-21 22:48:46 +0530

    Projects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import Example from "./Example";
+
 function App() {
   return (
-    <div className="anas">
-      <div className="muskan">
-asdfhkaljhsdfjashadsfasdf
-<Example/>
-      </div>
-        THIS IS APP COMPONENT
-    </div>
+    <>
+
+    </>
   );
 }
 
 export default App;
+
+
