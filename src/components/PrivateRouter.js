e4061b0b12a7020898f9d65377d081169afe1f2c
abbasmurudkar <abbasmurudkar18@icloud.com>
2021-12-21 23:16:42 +0530

    Projects

--- src/components/PrivateRouter.js
+++ src/components/PrivateRouter.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function StartupPage() {
+function PrivateRouter() {
     return (
         <div>
             
         </div>
     )
 }
-export default StartupPage
+
+export default PrivateRouter
--- src/components/PublicRouter.js
+++ src/components/PublicRouter.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function StartupPage() {
+function PublicRouter() {
     return (
         <div>
             
         </div>
     )
 }
-export default StartupPage
+
+export default PublicRouter
