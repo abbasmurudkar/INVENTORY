6131f8427d16309e2f428d2a65b210abde58473c
abbasmurudkar <abbasmurudkar18@icloud.com>
2022-05-25 20:07:28 +0530

    Razorpay-Payment

--- src/components/ADDTIONAL COMPONENT/Active-Carosuel.js
+++ src/components/ADDTIONAL COMPONENT/Active-Carosuel.js
@@ -101,6 +101,4 @@ button{
   }
 }
 `
-const Comp = styled.div`
-color:red;
-`
+
--- src/components/DasboardList/SubFiles/ShowSymbol.js
+++ src/components/DasboardList/SubFiles/ShowSymbol.js
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Alert, Button, List } from 'rsuite'
